4ed13f742941e65bf3063ac955333f204aea52f3
Alexey <exelay.rapik@gmail.com>
2021-01-07 19:56:18 +0300

    Update mongo pipeline for hourly scraping

--- carspass/pipelines.py
+++ carspass/pipelines.py
@@ -1,22 +1,24 @@
 import pymongo
-from itemadapter import ItemAdapter
 
 
 class MongodbPipeline:
 
     client = None
     db = None
+    collection = None
 
     def open_spider(self, spider):
         self.client = pymongo.MongoClient(
             "mongodb+srv://imdb:rowdy0987@carspass.mskrx.mongodb.net/<dbname>?retryWrites=true&w=majority"
         )
         self.db = self.client["Carspass"]
+        self.collection = self.db[spider.scraping_time]
 
     def close_spider(self, spider):
         self.client.close()
 
     def process_item(self, item, spider):
-        collection_name = spider.token
-        self.db[collection_name].insert_one(ItemAdapter(item).asdict())
+        collection_name = spider.scraping_time
+        if not self.collection.count_documents({'id': item['id']}):
+            self.db[collection_name].insert_one(item)
         return item
