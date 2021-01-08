b01090a3b8e2ec68a2a487200bb20a7ccd395eb7
Alexey <exelay.rapik@gmail.com>
2021-01-08 16:36:07 +0300

    Update avito scraper for hourly scraping

--- carspass/spiders/avito.py
+++ carspass/spiders/avito.py
@@ -1,5 +1,6 @@
 import json
 import logging
+from datetime import datetime
 from requests import PreparedRequest
 
 import scrapy
@@ -8,50 +9,19 @@ import scrapy
 class AvitoSpider(scrapy.Spider):
     name = 'avito'
     allowed_domains = ['avito.ru']
-    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_0) AppleWebKit/537.36 (KHTML, like Gecko) " \
-                 "Chrome/86.0.4240.198 Safari/537.36"
+
     url = str()
     page_number = 1
     proxy_url = "https://app.scrapingbee.com/api/v1/"
 
-    def __init__(self, *args, **kwargs):
-        super(AvitoSpider, self).__init__(*args, **kwargs)
-        self.token = getattr(self, "token")
-
-        self.brand = getattr(self, "brand", None)
-        self.model = getattr(self, "model", None)
-
-        self.city = getattr(self, "city", 'sankt-peterburg')
-        self.price_min = getattr(self, "price_min", None)
-        self.price_max = getattr(self, "price_max", None)
-        self.year_min = getattr(self, "year_min", None)
-        self.year_max = getattr(self, "year_max", None)
-        self.transmission = getattr(self, "transmission", None)
-        self.v_min = getattr(self, "v_min", None)
-        self.v_max = getattr(self, "v_max", None)
-        self.radius = getattr(self, "radius", None)
-        self.steering_w = getattr(self, "steering_w", None)
-        self.car_body = getattr(self, "car_body", None)
-        self.vendor = getattr(self, "vendor", None)
-        self.latest_ads = getattr(self, "latest_ads", None)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.scraping_time = datetime.today().date().isoformat()
 
     def start_requests(self):
         req = PreparedRequest()
-        params = {
-            'pmin': self.price_min,
-            'pmax': self.price_max,
-            'radius': self.radius,
-        }
-        if self.brand and self.model:
-            abs_url = f"https://www.avito.ru/{self.city}/avtomobili/s_probegom/{self.brand}/{self.model}"
-        elif self.brand:
-            abs_url = f"https://www.avito.ru/{self.city}/avtomobili/s_probegom/{self.brand}"
-        elif self.vendor:
-            abs_url = f"https://www.avito.ru/{self.city}/avtomobili/s_probegom/{self.vendor}"
-        else:
-            abs_url = f"https://www.avito.ru/{self.city}/avtomobili/s_probegom/"
-        req.prepare_url(abs_url, params)
-        self.url = req.url
+
+        self.url = 'https://www.avito.ru/sankt-peterburg/avtomobili/s_probegom-ASgBAgICAUSGFMjmAQ?cd=1&s=104'
         proxy_params = {
             "api_key": "EHW1NW8Y19PCOMPHBDARWQ2A1BOS6GIDEP9ZBWAWUXX6BUE0PIIL94PUW813WY6LISV941770L7R2U4B",
             "url": self.url,
@@ -60,7 +30,7 @@ class AvitoSpider(scrapy.Spider):
         req.prepare_url(self.proxy_url, proxy_params)
         url = req.url
 
-        yield scrapy.Request(url=url, headers={'User-Agent': self.user_agent}, callback=self.parse_item)
+        yield scrapy.Request(url=url, callback=self.parse_item)
 
     @staticmethod
     def get_id(ad):
@@ -153,5 +123,5 @@ class AvitoSpider(scrapy.Spider):
         }
         req.prepare_url(self.proxy_url, proxy_params)
         url = req.url
-        if self.page_number <= 2:
+        if self.page_number <= 50:
             yield scrapy.Request(url=url, callback=self.parse_item, dont_filter=True)
