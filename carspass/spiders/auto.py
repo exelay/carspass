ba21113e01fe05fb9eb25a08933138b8b43e8948
Alexey <exelay.rapik@gmail.com>
2021-01-08 16:33:27 +0300

    Update autoru scraper for hourly scraping

--- carspass/spiders/auto.py
+++ carspass/spiders/auto.py
@@ -1,5 +1,5 @@
 import logging
-from requests import PreparedRequest
+from datetime import datetime
 
 import scrapy
 
@@ -8,45 +8,12 @@ class AutoSpider(scrapy.Spider):
     name = 'autoru'
     allowed_domains = ['auto.ru']
 
-    def __init__(self, *args, **kwargs):
-        super(AutoSpider, self).__init__(*args, **kwargs)
-        self.token = getattr(self, "token")
-
-        self.brand = getattr(self, "brand", None)
-        self.model = getattr(self, "model", None)
-
-        self.city = getattr(self, "city", "sankt-peterburg")
-        self.price_min = getattr(self, "price_min", None)
-        self.price_max = getattr(self, "price_max", None)
-        self.year_min = getattr(self, "year_min", None)
-        self.year_max = getattr(self, "year_max", None)
-        self.transmission = getattr(self, "transmission", None)
-        self.v_min = getattr(self, "v_min", None)
-        self.v_max = getattr(self, "v_max", None)
-        self.radius = getattr(self, "radius", None)
-        self.steering_w = getattr(self, "steering_w", None)
-        self.car_body = getattr(self, "car_body", None)
-        self.vendor = getattr(self, "vendor", None)
-        self.latest_ads = getattr(self, "latest_ads", None)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.scraping_time = datetime.today().date().isoformat()
 
     def start_requests(self):
-        req = PreparedRequest()
-        params = {
-            'pmin': self.price_min,
-            'pmax': self.price_max,
-            'radius': self.radius,
-        }
-        if self.brand and self.model:
-            abs_url = f"https://auto.ru/{self.city}/cars/{self.brand}/{self.model}/used/"
-        elif self.brand:
-            abs_url = f"https://auto.ru/{self.city}/cars/{self.brand}/used/"
-        elif self.vendor:
-            abs_url = f"https://auto.ru/{self.city}/cars/{self.vendor}/used/"
-        else:
-            abs_url = f"https://auto.ru/{self.city}/cars/used/"
-        req.prepare_url(abs_url, params)
-        url = req.url
-
+        url = 'https://auto.ru/sankt-peterburg/cars/used/?page=99&sort=cr_date-desc&top_days=1'
         yield scrapy.Request(url=url, callback=self.parse_item)
 
     @staticmethod
@@ -129,6 +96,5 @@ class AutoSpider(scrapy.Spider):
             }
 
         next_page = response.xpath('//a[contains(@class, "ListingPagination-module__next")]/@href').get()
-        page_number = int(next_page.split('/')[-1][-1])
-        if next_page and page_number <= 5:
+        if next_page:
             yield scrapy.Request(url=next_page, callback=self.parse_item)
