5d0290112846112f53dc5c1c2561ada68ab3b36b
Alexey <exelay.rapik@gmail.com>
2021-01-13 13:40:00 +0300

    Updated avito spider

--- carspass/spiders/avito.py
+++ carspass/spiders/avito.py
@@ -1,5 +1,7 @@
 import json
+import yaml
 import logging
+from datetime import datetime
 from requests import PreparedRequest
 
 import scrapy
@@ -41,6 +43,14 @@ class AvitoSpider(scrapy.Spider):
             logging.debug(f"Failed to get image link. {e}")
 
     @staticmethod
+    def get_publish_date(ad):
+        try:
+            unix_time = int(ad['sortTimeStamp']) // 1000
+            return datetime.fromtimestamp(unix_time).isoformat()
+        except Exception as e:
+            logging.debug(f"Failed to get publish date. {e}")
+
+    @staticmethod
     def get_title(ad):
         try:
             return ad['title']
@@ -58,7 +68,8 @@ class AvitoSpider(scrapy.Spider):
     def get_mileage(ad):
         try:
             payload = ad['iva']['AutoParamsStep'][0]['payload']['text']
-            return payload.split(',')[0][:-2].replace(' ', '')
+            mileage = payload.split(',')[0][:-2].replace(' ', '')
+            return int(mileage)
         except Exception as e:
             logging.debug(f"Failed to get mileage. {e}")
 
@@ -88,6 +99,71 @@ class AvitoSpider(scrapy.Spider):
         except Exception as e:
             logging.debug(f"Failed to get link. {e}")
 
+    @staticmethod
+    def get_brand(ad):
+        try:
+            url = ad["urlPath"]
+            car_metadata = url.split('/')[-1].split('_')
+            return car_metadata[0]
+        except Exception as e:
+            logging.debug(f"Failed to get brand. {e}")
+
+    @staticmethod
+    def get_model(ad):
+        try:
+            url = ad["urlPath"]
+            car_metadata = url.split('/')[-1].split('_')
+            return car_metadata[1]
+        except Exception as e:
+            logging.debug(f"Failed to get model. {e}")
+
+    @staticmethod
+    def get_year(ad):
+        try:
+            url = ad["urlPath"]
+            car_metadata = url.split('/')[-1].split('_')
+            return int(car_metadata[2])
+        except Exception as e:
+            logging.debug(f"Failed to get year. {e}")
+
+    def get_transmission(self, ad):
+        try:
+            car_data = ad['iva']['AutoParamsStep'][0]['payload']['text']
+            transmission = car_data.split()[4].lower()
+            with open(f'conventions/{self.name}.yaml') as f:
+                transmissions = yaml.load(f, Loader=yaml.FullLoader)['transmission']
+            return transmissions[transmission]
+        except Exception as e:
+            logging.debug(f"Failed to get transmission. {e}")
+
+    def get_frame_type(self, ad):
+        try:
+            car_data = ad['iva']['AutoParamsStep'][0]['payload']['text']
+            frame_type = car_data.split()[7].strip(',')
+            with open(f'conventions/{self.name}.yaml') as f:
+                frame_types = yaml.load(f, Loader=yaml.FullLoader)['frame_type']
+            return frame_types[frame_type]
+        except Exception as e:
+            logging.debug(f"Failed to get frame type. {e}")
+
+    @staticmethod
+    def get_power(ad):
+        try:
+            car_data = ad['iva']['AutoParamsStep'][0]['payload']['text']
+            power = car_data.split()[5].strip('(')
+            return int(power)
+        except Exception as e:
+            logging.debug(f"Failed to get power. {e}")
+
+    @staticmethod
+    def get_volume(ad):
+        try:
+            car_data = ad['iva']['AutoParamsStep'][0]['payload']['text']
+            volume = car_data.split()[3]
+            return float(volume)
+        except Exception as e:
+            logging.debug(f"Failed to get volume. {e}")
+
     def parse_item(self, response):
         p_loader = json.loads(response.xpath('//div[@class="js-initial"]/@data-state').get())
         ads = [item for item in p_loader['catalog']['items'] if item['type'] == 'item']
@@ -95,14 +171,22 @@ class AvitoSpider(scrapy.Spider):
             yield {
                 'id': self.get_id(ad),
                 'img_link': self.get_img_link(ad),
-                'publish_date': None,
+                'publish_date': self.get_publish_date(ad),
                 'title': self.get_title(ad),
                 'price': self.get_price(ad),
                 'mileage': self.get_mileage(ad),
                 'tech_info': self.get_tech_info(ad),
                 'location': self.get_location(ad),
+                'brand': self.get_brand(ad),
+                'model': self.get_model(ad),
+                'year': self.get_year(ad),
+                'transmission': self.get_transmission(ad),
+                'frame_type': self.get_frame_type(ad),
+                'power': self.get_power(ad),
+                'volume': self.get_volume(ad),
                 'metro': None,
                 'link': self.get_link(ad),
                 'actual': True,
-                'source': 'avito',
+                'source': self.name,
+                'scraped_at': datetime.now().isoformat(),
             }
