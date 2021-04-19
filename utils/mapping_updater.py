c096a55853ddc208d2a2007c3559b41031e6e8bd
exelay <exelay.rapik@gmail.com>
2021-04-19 17:24:57 +0300

    Updated mapping updater

--- utils/mapping_updater.py
+++ utils/mapping_updater.py
@@ -5,9 +5,6 @@ import pymongo
 from config import MONGODB_URI
 
 
-MONGODB_URI = "mongodb://root:Y4QbE5nHqP46QzvT@62.113.117.27:27017/?authSource=admin&readPreference=primary" \
-            "&appname=MongoDB%20Compass&ssl=false"
-
 sites = ('avito', 'amru', 'drom', 'autoru')
 
 
